ff8a81d2a75920273882fbf4b9f2f909d934acf5
Harshit Patel <hsp2810@gmail.com>
2025-04-22 23:12:35 -0600

    github link

--- modules/landing/ui/footer.tsx
+++ modules/landing/ui/footer.tsx
@@ -1,12 +1,12 @@
-import { Heart } from "lucide-react";
+import { Github, Heart } from "lucide-react";
 
 export function Footer() {
   return (
     <footer className='my-8 border-t pt-8 flex flex-col gap-4 md:flex-row md:items-center md:justify-between px-10'>
-      <p className='text-xs text-muted-foreground text-center md:text-left'>
+      <p className='text-sm text-muted-foreground text-center md:text-left'>
         © 2025 BetterAuthApplication. All rights reserved.
       </p>
-      <p className='flex justify-center text-xs text-muted-foreground text-center md:text-left'>
+      <p className='flex justify-center text-sm text-muted-foreground text-center md:text-left'>
         Built with{" "}
         <a
           target='_blank'
@@ -16,6 +16,16 @@ export function Footer() {
           Harshit Patel
         </a>{" "}
         with Love <Heart className='size-4 fill-rose-400' />
+      </p>
+      <p className='flex justify-center text-sm text-muted-foreground text-center md:text-left'>
+        <a
+          target='_blank'
+          href='https://github.com/hsp2810/BetterauthImplementation'
+          className='mx-1 hover:underline flex gap-1 items-center'
+        >
+          <Github className='size-4' />
+          Source code
+        </a>
       </p>
     </footer>
   );
