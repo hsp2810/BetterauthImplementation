fd3ce4ceffdeb184cdde0c7f4e5415ab3c3a503d
Harshit Patel <hsp2810@gmail.com>
2025-04-22 20:31:05 -0600

    added production url

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,6 +2,6 @@ import { createAuthClient } from "better-auth/react";
 import { adminClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.PRODUCTION_URL,
   plugins: [adminClient()],
 });
