bad57a1c7a8159326cf982d18f114f69666de470
Harshit Patel <hsp2810@gmail.com>
2025-04-22 20:06:26 -0600

    disable eslint rules

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
-import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db";
 import { schema } from "@/db/schema";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
 import { admin } from "better-auth/plugins";
 import { sendEmail, sendResetPasswordEmail } from "./email";
